3197ffb0c6fc8b97e0ade9a726bc0ac569c327e5
nathank07 <nathank7256@gmail.com>
2024-12-02 02:02:59 -0600

    Completed part 2

--- 02/main.hs
+++ 02/main.hs
@@ -13,23 +13,42 @@ readInput h = do
             rest <- readInput h
             return $ nums : rest
 
-data IsSafe = Asc | Desc | None
+-- What a gross solution :/
+
+data IsSafe = AscSafe | AscUnsafe | DescSafe | DescUnsafe | None | NoneUnsafe
+
+numsSafe :: Int -> Int -> IsSafe -> Bool
+numsSafe x1 x2 safety 
+    | x1 == x2 = False
+    | otherwise = case safety of
+                    AscSafe    -> x2 - x1 <= 3 && x2 - x1 > 0
+                    DescSafe   -> x1 - x2 <= 3 && x1 - x2 > 0
+                    AscUnsafe  -> numsSafe x1 x2 AscSafe
+                    DescUnsafe -> numsSafe x1 x2 DescSafe
+                    None       -> numsSafe x1 x2 AscSafe || numsSafe x1 x2 DescSafe
+                    NoneUnsafe -> numsSafe x1 x2 None
 
 safeReport :: [Int] -> IsSafe -> Bool
 safeReport []  _ = True
 safeReport [x] _ = True
-safeReport (x1:x2:xs) safety
-    | x1 == x2 = False
-    | otherwise = case safety of
-        None -> safeReport ([x1, x2] ++ xs) (if x1 < x2 then Asc else Desc)
-        Asc  -> x2 - x1 <= 3 && x2 - x1 > 0 && safeReport (x2:xs) Asc
-        Desc -> x1 - x2 <= 3 && x1 - x2 > 0 && safeReport (x2:xs) Desc
+safeReport (x1:x2:xs) safety =
+    case safety of
+        NoneUnsafe -> safe && safeReport (x2:xs) (if x1 < x2 then AscUnsafe else DescUnsafe)
+        AscUnsafe  -> safe && safeReport (x2:xs) AscUnsafe
+        DescUnsafe -> safe && safeReport (x2:xs) DescUnsafe
+        None ->       safe && safeReport (x2:xs) (if x1 < x2 then AscSafe else DescSafe) || safeReport (x1:xs) NoneUnsafe || safeReport (x2:xs) NoneUnsafe
+        AscSafe ->    safe && safeReport (x2:xs) AscSafe  || safeReport (x1:xs) AscUnsafe
+        DescSafe ->   safe && safeReport (x2:xs) DescSafe || safeReport (x1:xs) DescUnsafe
+        where safe = numsSafe x1 x2 safety
 
-safeReports :: [[Int]] -> Int
-safeReports xs = length $ filter id $ map (`safeReport` None) xs
+
+safeReports :: [[Int]] -> Bool -> Int
+safeReports xs dampen = length $ filter id $ map (`safeReport` if dampen then None else NoneUnsafe) xs
 
 main :: IO ()
 main = do
     h <- openFile "input" ReadMode
     x <- readInput h
-    print $ safeReports x
+    print $ safeReports x False
+    print $ safeReports x True
+
