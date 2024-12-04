7bbf4a276d2b2b7bc13ac824bc7f7460a385d307
nathank07 <nathank7256@gmail.com>
2024-12-04 01:40:44 -0600

    Cleaned up

--- 04/main.hs
+++ 04/main.hs
@@ -16,15 +16,15 @@ data Direction = Across | Below | BelowLeft | BelowRight
 getDirection :: String -> [String] -> Direction -> String
 getDirection searchStr belowStrs Across = take 3 $ drop 1 searchStr
 getDirection searchStr belowStrs direction = getIdxs xs belowStrs
-        where 
+        where
             xs = case direction of
                     Below -> replicate 3 strLoc
                     BelowLeft -> [strLoc - x | x <- [1..3]]
                     BelowRight -> [strLoc + x | x <- [1..3]]
-            strLoc = if length belowStrs > 0 then length (head belowStrs) - length searchStr else 0
+            strLoc = if not (null belowStrs) then length (head belowStrs) - length searchStr else 0
             getIdxs (x:xs) (y:ys) = if x >= 0 && x < length y then (y !! x) : getIdxs xs ys else ""
             getIdxs _ _ = ""
-        
+
 
 getXmas :: [String] -> Int
 getXmas ([]:xss) = getXmas xss
@@ -32,23 +32,23 @@ getXmas ((x:xs):xss) = case x of
     'S' -> getXmas (xs:xss) + hasCrosswordMatch "AMX"
     'X' -> getXmas (xs:xss) + hasCrosswordMatch "MAS"
     _ -> getXmas (xs:xss)
-    where 
-        hasCrosswordMatch s = length $ filter id [fn Across == s, fn Below == s, fn BelowLeft == s, fn BelowRight == s]
+    where
+        hasCrosswordMatch s = length $ filter id $ map (==s) [fn Across, fn Below, fn BelowLeft, fn BelowRight]
         fn = getDirection (x:xs) xss
 getXmas _ = 0
 
 getXXmas :: [String] -> Int
 getXXmas ([]:xss) = getXXmas xss
 getXXmas ((x:xs):xss) = getXXmas (xs:xss) + isXXMas
-    where isXXMas = case (leftRight, rightLeft) of
+    where
+        isXXMas = case (leftRight, rightLeft) of
             ("SAM", "SAM") -> 1
             ("MAS", "SAM") -> 1
             ("MAS", "MAS") -> 1
             ("SAM", "MAS") -> 1
             _ -> 0
-          leftRight = x : take 2 (getDirection (x:xs) xss BelowRight)
-          rightLeft = head (tail xs) : take 2 (getDirection (tail xs) xss BelowLeft)
-
+        leftRight = x : take 2 (getDirection (x:xs) xss BelowRight)
+        rightLeft = head (tail xs) : take 2 (getDirection (tail xs) xss BelowLeft)
 getXXmas _ = 0
 
 main :: IO()
