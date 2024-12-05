73d7827292948d6f432a32f8f1095a22d3ed2903
nathank07 <nathank7256@gmail.com>
2024-12-05 01:57:07 -0600

    Completed part 2

--- 05/main.hs
+++ 05/main.hs
@@ -1,5 +1,6 @@
 import GHC.IO.Handle
 import System.IO
+import Data.List
 
 
 -- https://stackoverflow.com/a/7569301
@@ -33,19 +34,23 @@ readPageOrders endHandle = do
             let nums = map (read :: String -> Int) $ splitBy ',' line
             return (nums : rest)
 
-intersect :: Order -> Order -> Order
-intersect [] _ = []
-intersect _ [] = []
-intersect xs ys = filter (`elem` xs) ys
-
 checkPageOrder :: Rules -> Order -> Bool
 checkPageOrder r [] = True
 checkPageOrder r (x:xs) = isBehindEach && checkPageOrder r xs
     where isBehindEach = null $ intersect xs (map fst $ filter (\(a, b) -> b == x) r)
+
+correctPageOrder :: Rules -> Order -> Order
+correctPageOrder r o = sortBy (\a b -> compare (priority a) (priority b)) o
+    where priority x = length $ filter (\(a, b) -> x == b && elem a o) r
 
 main :: IO()
 main = do
     h <- openFile "input" ReadMode
     rules <- readPageRules h
     orders <- readPageOrders h
+    -- Part 1
     print $ sum $ map (\xs -> xs !! (length xs `div` 2)) $ filter (checkPageOrder rules) orders
+    -- Part 2
+    -- Tests correctPageOrder by getting all incorrect ones, correcting them, and seeing if they are correct
+    print $ all (checkPageOrder rules . correctPageOrder rules) (filter (not . checkPageOrder rules) orders)
+    print $ sum $ map ((\xs -> xs !! (length xs `div` 2)) . correctPageOrder rules) (filter (not . checkPageOrder rules) orders)
